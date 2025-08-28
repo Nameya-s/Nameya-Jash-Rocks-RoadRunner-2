5ba47c5605b9852448e42fe974ad88fa0dd44773
Nameya <nameyasojan@gmail.com>
2025-08-28 17:59:37 -0400

    Did the forward Push test, and added the in pertick value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -63,7 +63,7 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
         // drive model parameters
-        public double inPerTick = 1;
+        public double inPerTick = 0.00232517584;
         public double lateralInPerTick = inPerTick;
         public double trackWidthTicks = 0;
 
