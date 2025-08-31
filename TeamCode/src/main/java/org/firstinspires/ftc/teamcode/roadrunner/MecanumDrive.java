ea5d55cb969635238b1fb023f2c8a46a0830914c
darshanvaishnav <jash.vaishnav@gmail.com>
2025-08-31 14:14:51 -0400

    added kv and ks values to code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -68,8 +68,8 @@ public final class MecanumDrive {
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 1.9064993576960196;
+        public double kV = 0.00034383207475803816;
         public double kA = 0;
 
         // path profile parameters (in inches)
